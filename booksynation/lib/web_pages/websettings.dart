776f7326085971c1ad657b2f062583c027308e9f
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2021-10-01 21:08:27 +0800

    v5.7 Web Update

--- booksynation/lib/web_pages/websettings.dart
+++ booksynation/lib/web_pages/websettings.dart
@@ -1,6 +1,7 @@
 import 'package:booksynation/web_pages/webmanage.dart';
 import 'package:booksynation/web_pages/webmissed.dart';
 import 'package:booksynation/web_pages/webschedule.dart';
+import 'package:booksynation/weblogin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -26,6 +27,7 @@ class _WebSettingsState extends State<WebSettings> {
   @override
   Widget build(BuildContext context) {
     final name = 'Juan Dela Cruz';
+    final email = 'book-cine@gmail.com';
     final urlImage = 'images/nashimg.png';
     // final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
@@ -40,108 +42,152 @@ class _WebSettingsState extends State<WebSettings> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: height,
-                  width: 260,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.06),
-                        blurRadius: 18,
-                        offset: Offset(6, 0), // changes position of shadow
-                      ),
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.06),
-                        blurRadius: 18,
-                        offset: Offset(6, 0), // changes position of shadow
-                      ),
-                    ],
+            Container(
+              height: height,
+              width: 260,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.06),
+                    blurRadius: 18,
+                    offset: Offset(6, 0), // changes position of shadow
                   ),
-                  child: Column(
-                    children: [
-                      Container(
-                        child: Column(
-                          children: [
-                            Container(
-                              height: height * 0.2,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  CircleAvatar(
-                                    radius: 40,
-                                    backgroundColor: Colors.white,
-                                    backgroundImage: AssetImage(urlImage),
-                                  ),
-                                  SizedBox(
-                                    height: 20,
-                                  ),
-                                  Text(name,
-                                      style: TextStyle(
-                                          fontSize: 16,
-                                          color: Color(0xFF3DDD6A))),
-                                  const SizedBox(
-                                    height: 2,
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(height: 20),
-                            buildMenuItem(
-                              text: 'Manage Vaccines',
-                              icon: Icons.hourglass_bottom_outlined,
-                              onClicked: () => selectedItem(context, 0),
-                            ),
-                            const SizedBox(height: 12),
-                            buildMenuItem(
-                              text: 'Scheduled Vaccinations',
-                              icon: Icons.phone_outlined,
-                              onClicked: () => selectedItem(context, 1),
-                            ),
-                            const SizedBox(height: 12),
-                            buildMenuItem(
-                              text: 'Missed Vaccinations',
-                              icon: Icons.payment_outlined,
-                              onClicked: () => selectedItem(context, 2),
-                            ),
-                            const SizedBox(height: 12),
-                            buildMenuItem(
-                              text: 'Account Settings',
-                              icon: Icons.settings_outlined,
-                              onClicked: () => selectedItem(context, 3),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Spacer(),
-                      Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SvgPicture.asset('images/Logo_BSN.svg', height: 20),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              'BookSyNation',
-                              style: TextStyle(
-                                fontFamily: 'Antic Didone',
-                                fontWeight: FontWeight.bold,
-                                fontSize: 12,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                    ],
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.06),
+                    blurRadius: 18,
+                    offset: Offset(6, 0), // changes position of shadow
                   ),
-                )
-              ],
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Container(
+                    width: 260,
+                    height: 22,
+                    child: Row(children: [
+                      const SizedBox(
+                        width: 3,
+                      ),
+                      Text("BookSyNation",
+                          style: TextStyle(
+                              fontFamily: 'Antic Didone',
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF58BCA4))),
+                    ]),
+                  ),
+                  Container(
+                    height: (height * 0.9) - 25,
+                    width: 260,
+                    child: Material(
+                      color: Colors.white70,
+                      child: ListView(
+                        children: <Widget>[
+                          Container(
+                            child: Column(
+                              children: [
+                                Container(
+                                  width: 260,
+                                  height: height * 0.2,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      CircleAvatar(
+                                        radius: 20,
+                                        backgroundColor: Colors.white,
+                                        backgroundImage: AssetImage(urlImage),
+                                      ),
+                                      SizedBox(
+                                        width: 20,
+                                      ),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(name,
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  color: Color(0xFF192A3E))),
+                                          const SizedBox(
+                                            height: 2,
+                                          ),
+                                          Text(email,
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  color: Color(0xFF90A0B7))),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                buildMenuItem(
+                                  text: 'Manage Vaccines',
+                                  icon: SvgPicture.asset(
+                                      'images/vaccine_icon.svg',
+                                      height: 20),
+                                  onClicked: () => selectedItem(context, 0),
+                                ),
+                                const SizedBox(height: 12),
+                                buildMenuItem(
+                                  text: 'Scheduled Vaccinations',
+                                  icon: SvgPicture.asset(
+                                      'images/schedule_icon.svg',
+                                      height: 20),
+                                  onClicked: () => selectedItem(context, 1),
+                                ),
+                                const SizedBox(height: 12),
+                                buildMenuItem(
+                                  text: 'Missed Vaccinations',
+                                  icon: SvgPicture.asset(
+                                      'images/missed_icon.svg',
+                                      height: 20),
+                                  onClicked: () => selectedItem(context, 2),
+                                ),
+                                const SizedBox(height: 12),
+                                buildMenuItem(
+                                  text: 'Account Settings',
+                                  icon: SvgPicture.asset(
+                                      'images/settings_icon.svg',
+                                      height: 20),
+                                  onClicked: () => selectedItem(context, 3),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 260,
+                    height: height * 0.1,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 260,
+                          child: signOutMenu(
+                            text: 'Sign-out',
+                            icon: SvgPicture.asset('images/signout_icon.svg',
+                                height: 20),
+                            onClicked: () => selectedItem(context, 4),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             )
           ],
         ),
@@ -151,16 +197,30 @@ class _WebSettingsState extends State<WebSettings> {
 
   Widget buildMenuItem({
     required String text,
-    required IconData icon,
+    required SvgPicture icon,
     VoidCallback? onClicked,
   }) {
     final color = Colors.black;
-    final hoverColor = Colors.grey.withOpacity(0.5);
+    final hoverColor = Colors.grey[200];
 
     return ListTile(
-      leading: Icon(icon, color: color),
+      leading: icon,
       title: Text(text, style: TextStyle(color: color)),
       hoverColor: hoverColor,
+      onTap: onClicked,
+    );
+  }
+
+  Widget signOutMenu({
+    required String text,
+    required SvgPicture icon,
+    VoidCallback? onClicked,
+  }) {
+    final color = Colors.black;
+
+    return ListTile(
+      leading: icon,
+      title: Text(text, style: TextStyle(color: color)),
       onTap: onClicked,
     );
   }
@@ -187,6 +247,11 @@ class _WebSettingsState extends State<WebSettings> {
       case 3:
         Navigator.of(context).push(MaterialPageRoute(
           builder: (context) => WebSettings(),
+        ));
+        break;
+      case 4:
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => WebLogin(),
         ));
         break;
     }
