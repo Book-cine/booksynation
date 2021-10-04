a4cbca911befee7cfbfa5f69cbd01e1949057d3f
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2021-10-04 16:15:41 +0800

    v6.32 Resposiveness Fix

--- booksynation/lib/web_pages/webschedule.dart
+++ booksynation/lib/web_pages/webschedule.dart
@@ -18,77 +18,79 @@ class _WebScheduleState extends State<WebSchedule> {
     return Container(
         height: height,
         width: width,
-        child: Stack(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Positioned(
-              top: 20,
-              left: width * 0.03,
-              child: Container(
-                height: 40,
-                width: width - (width * 0.03),
-                child: Row(
-                  children: [
-                    Container(
-                      child: Text('Vaccine: ',
-                          style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.w600,
-                              fontSize: 13,
-                              color: Color(0xFF334D6E))),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Container(
-                      child: DropdownButton<String>(
-                        value: dropdownValue,
-                        icon: const Icon(Icons.arrow_downward),
-                        iconSize: 18,
-                        elevation: 16,
-                        style: const TextStyle(color: Color(0xFF109CF1)),
-                        underline: Container(
-                          height: 2,
-                          color: Color(0xFFCCCCCC),
-                        ),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownValue = newValue!;
-                          });
-                        },
-                        items: <String>[
-                          'All',
-                          'Astrazenica',
-                          'Janssen',
-                          'Moderna',
-                          'Pfizer',
-                          'Sinovac'
-                        ].map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value,
-                                style: TextStyle(
-                                    fontFamily: 'Poppins',
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 13,
-                                    color: Color(0xFF109CF1))),
-                          );
-                        }).toList(),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 60,
+              width: width - (width * 0.03),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text('Vaccine: ',
+                        style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
+                            fontSize: 13,
+                            color: Color(0xFF334D6E))),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    child: DropdownButton<String>(
+                      value: dropdownValue,
+                      icon: const Icon(Icons.arrow_downward),
+                      iconSize: 18,
+                      elevation: 16,
+                      style: const TextStyle(color: Color(0xFF109CF1)),
+                      underline: Container(
+                        height: 2,
+                        color: Color(0xFFCCCCCC),
                       ),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      },
+                      items: <String>[
+                        'All',
+                        'Astrazenica',
+                        'Janssen',
+                        'Moderna',
+                        'Pfizer',
+                        'Sinovac'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value,
+                              style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 13,
+                                  color: Color(0xFF109CF1))),
+                        );
+                      }).toList(),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              child: Container(
-                height: height - 50,
-                width: width,
-                child: Center(
-                  child: Container(
-                    width: width - (width * 0.03) - 30,
-                    height: height - 100,
+            Container(
+              height: height - 100,
+              width: width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: width - (width * 0.035),
+                    height: height - 120,
                     decoration: BoxDecoration(
                       color: Color(0xFFF7F7F7),
                       boxShadow: [
@@ -99,94 +101,105 @@ class _WebScheduleState extends State<WebSchedule> {
                         )
                       ],
                     ),
-                    child: DataTable(
-                      columns: const <DataColumn>[
-                        DataColumn(
-                          label: Text(
-                            'Unique ID',
-                            style: TextStyle(fontStyle: FontStyle.italic),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: DataTable(
+                        columnSpacing: width / 1000,
+                        columns: const <DataColumn>[
+                          DataColumn(
+                            label: Text(
+                              'Unique ID',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
                           ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'Name',
-                            style: TextStyle(fontStyle: FontStyle.italic),
+                          DataColumn(
+                            label: Text(
+                              'Name',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
                           ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'Email',
-                            style: TextStyle(fontStyle: FontStyle.italic),
+                          DataColumn(
+                            label: Text(
+                              'Email',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
                           ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'Vaccine',
-                            style: TextStyle(fontStyle: FontStyle.italic),
+                          DataColumn(
+                            label: Text(
+                              'Vaccine',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
                           ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'Dosage',
-                            style: TextStyle(fontStyle: FontStyle.italic),
+                          DataColumn(
+                            label: Text(
+                              'Dosage',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
                           ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'Category',
-                            style: TextStyle(fontStyle: FontStyle.italic),
+                          DataColumn(
+                            label: Text(
+                              'Category',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
                           ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'Date Scheduled',
-                            style: TextStyle(fontStyle: FontStyle.italic),
+                          DataColumn(
+                            label: Text(
+                              'Date Scheduled',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
                           ),
-                        ),
-                        DataColumn(
-                          label: Text(''),
-                        ),
-                      ],
-                      rows: <DataRow>[
-                        DataRow(
-                          cells: <DataCell>[
-                            DataCell(Text('19-4097-770')),
-                            DataCell(Text('Nash Uriel A. Tapayan')),
-                            DataCell(Text('tnashuriel@gmail.com')),
-                            DataCell(Text('Astrazenica')),
-                            DataCell(Text('2nd')),
-                            DataCell(Text('A4')),
-                            DataCell(Text('Sept 23, 2021')),
-                            DataCell(Row(
-                              children: [
-                                IconButton(
-                                  icon: SvgPicture.asset(
-                                    'images/check_icon.svg',
-                                    color: Color(0xFF4ECB71),
-                                    height: 25,
-                                    width: 25,
+                          DataColumn(
+                            label: Text(''),
+                          ),
+                        ],
+                        rows: <DataRow>[
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(Container(child: Text('19-4097-770'))),
+                              DataCell(Container(
+                                  child: Text('Nash Uriel A. Tapayan'))),
+                              DataCell(Container(
+                                  child: Text('tnashuriel@gmail.com'))),
+                              DataCell(Container(child: Text('Astrazenica'))),
+                              DataCell(Container(child: Text('2nd'))),
+                              DataCell(Container(child: Text('A4'))),
+                              DataCell(Container(child: Text('Sept 23, 2021'))),
+                              DataCell(
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      IconButton(
+                                        icon: SvgPicture.asset(
+                                          'images/check_icon.svg',
+                                          color: Color(0xFF4ECB71),
+                                          height: 25,
+                                          width: 25,
+                                        ),
+                                        tooltip: 'Mark as finished',
+                                        onPressed: () {},
+                                      ),
+                                      IconButton(
+                                        icon: SvgPicture.asset(
+                                          'images/reject_icon.svg',
+                                          color: Color(0xFFF11010),
+                                          height: 25,
+                                          width: 25,
+                                        ),
+                                        tooltip: 'Mark as missed',
+                                        onPressed: () {},
+                                      ),
+                                    ],
                                   ),
-                                  tooltip: 'Mark as finished',
-                                  onPressed: () {},
                                 ),
-                                IconButton(
-                                  icon: SvgPicture.asset(
-                                    'images/reject_icon.svg',
-                                    color: Color(0xFFF11010),
-                                    height: 25,
-                                    width: 25,
-                                  ),
-                                  tooltip: 'Mark as missed',
-                                  onPressed: () {},
-                                ),
-                              ],
-                            )),
-                          ],
-                        ),
-                      ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
             ),
           ],
