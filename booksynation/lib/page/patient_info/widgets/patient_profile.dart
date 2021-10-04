5c3dea6cf912e1d5ab6a87159970ce23c9ebec90
Mervin John C. Tampus <79685609+jdesolate@users.noreply.github.com>
2021-10-04 23:24:07 +0800

    v6.3.4 Patient Profile Form 10/04/21

--- booksynation/lib/page/patient_info/widgets/patient_profile.dart
+++ booksynation/lib/page/patient_info/widgets/patient_profile.dart
@@ -2,21 +2,17 @@ import 'package:booksynation/page/patient_info/widgets/display_data.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ProfileData extends InheritedWidget {
-  final String firstName;
-
-  const ProfileData({
-    Key? key,
-    required this.firstName,
-    required Widget child,
-  }) : super(key: key, child: child);
-
-  static String of(BuildContext context) =>
-      context.dependOnInheritedWidgetOfExactType<ProfileData>()!.firstName;
-
-  @override
-  bool updateShouldNotify(ProfileData old) => firstName != old.firstName;
-}
+Map<String, String> docFields = {
+  'firstname': '',
+  'middlename': '',
+  'lastname': '',
+  'suffix': '',
+  'age': '',
+  'bday': '',
+  'gender': '',
+  'civstatus': '',
+  'philhealth': '',
+};
 
 class PatientProfile extends StatefulWidget {
   const PatientProfile({
@@ -35,241 +31,198 @@ class PatientProfile extends StatefulWidget {
 class _PatientProfileState extends State<PatientProfile> {
   final Stream<QuerySnapshot> users =
       FirebaseFirestore.instance.collection('users').snapshots();
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => DisplayData(users: users)),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFF26A98A),
-                fixedSize: Size(
-                  widget.width * 0.45,
-                  widget.height * 0.060,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40.0),
-                ),
-              ),
-              child: Text(
-                'Display Data',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Mulish',
-                  fontWeight: FontWeight.w600,
-                  fontSize: widget.height * 0.018,
-                  decoration: TextDecoration.none,
-                ),
-              ),
-            ),
-            SizedBox(height: widget.height * 0.010),
-            MyCustomForm(height: widget.height, width: widget.width),
-          ],
-        ),
-      ],
-    );
-  }
-}
-
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({
-    Key? key,
-    @required this.height,
-    @required this.width,
-  }) : super(key: key);
-
-  final height;
-  final width;
-
-  @override
-  _MyCustomFormState createState() => _MyCustomFormState();
-}
-
-class _MyCustomFormState extends State<MyCustomForm> {
   final _formKey = GlobalKey<FormState>();
 
-  var firstName = '';
-  var middleName = '';
-  var lastName = '';
-  var suffix = '';
-  var age = '';
-  var philhealth = '';
   var gender = [
     "Male",
     "Female",
   ];
-  var sex = '';
   var civstatus = [
     "Single",
     "Married",
   ];
-  var civstat = '';
+
+  DateTime? date;
 
   @override
   Widget build(BuildContext context) {
-    CollectionReference users = FirebaseFirestore.instance.collection('users');
-    return ProfileData(
-      firstName: '',
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: PatientFormField(
-                    label: 'First Name',
-                  ),
-                ),
-                SizedBox(width: widget.width * 0.015),
-                Expanded(
-                  child: PatientFormField(
-                    label: 'Middle Name',
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: widget.height * 0.010),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  flex: 3,
-                  child: PatientFormField(
-                    label: 'Last Name',
-                  ),
-                ),
-                SizedBox(width: widget.width * 0.015),
-                Expanded(
-                  flex: 1,
-                  child: PatientFormField(
-                    label: 'Suffix',
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: widget.height * 0.010),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: PatientDropdown(
-                    dropList: gender,
-                  ),
-                ),
-                SizedBox(width: widget.width * 0.015),
-                Expanded(
-                  flex: 2,
-                  child: BirthdayPicker(),
-                ),
-                SizedBox(width: widget.width * 0.015),
-                Expanded(
-                  flex: 1,
-                  child: PatientFormField(
-                    label: 'Age',
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: widget.height * 0.010),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: PatientDropdown(
-                    dropList: civstatus,
-                  ),
-                ),
-                SizedBox(width: widget.width * 0.015),
-                Expanded(
-                  flex: 2,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(
-                        vertical: 13,
-                      ),
-                      labelText: 'PHIC/Philhealth No. (If Applicable)',
-                      labelStyle: TextStyle(fontSize: 12),
-                    ),
-                    onChanged: (value) {
-                      philhealth = value;
-                    },
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text(
-                          'Submitting Your Data',
-                        ),
-                      ),
-                    );
+    return _patientProfileForm(); // returns whole Profile form
+  }
 
-                    users
-                        .add({
-                          'firstname': firstName,
-                          'middlename': middleName,
-                          'lastname': lastName,
-                          'suffix': suffix,
-                          'gender': sex,
-                          'age': age,
-                          'civstatus': civstat,
-                          'philhealth': philhealth,
-                        })
-                        .then((value) => print('Add User'))
-                        .catchError(
-                            (error) => print('Failed to add user: $error'));
-                  }
-                },
-                child: const Text('Submit'),
+  Widget _patientProfileForm() {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    final Stream<QuerySnapshot<Object?>> user = users.snapshots();
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: _patientFormField('First Name'),
+              ),
+              SizedBox(width: widget.width * 0.015),
+              Expanded(
+                child: _patientFormField('Middle Name'),
+              ),
+            ],
+          ),
+          SizedBox(height: widget.height * 0.010),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 3,
+                child: _patientFormField('Last Name'),
+              ),
+              SizedBox(width: widget.width * 0.015),
+              Expanded(
+                flex: 1,
+                child: _patientFormField('Suffix'),
+              ),
+            ],
+          ),
+          SizedBox(height: widget.height * 0.010),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 1,
+                child: PatientDropdown(dropList: gender, label: 'Gender'),
+              ),
+              SizedBox(width: widget.width * 0.015),
+              Expanded(
+                flex: 2,
+                child: _birthdayPicker(),
+              ),
+              SizedBox(width: widget.width * 0.015),
+              Expanded(
+                flex: 1,
+                child: _patientFormField('Age'),
+              ),
+            ],
+          ),
+          SizedBox(height: widget.height * 0.010),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 1,
+                child: PatientDropdown(
+                  dropList: civstatus,
+                  label: 'Civil Status',
+                ),
+              ),
+              SizedBox(width: widget.width * 0.015),
+              Expanded(
+                flex: 2,
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 13,
+                    ),
+                    labelText: 'PHIC/Philhealth No. (If Applicable)',
+                    labelStyle: TextStyle(fontSize: 12),
+                  ),
+                  onChanged: (value) {
+                    docFields['philhealth'] = value;
+                  },
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                        'Submitting Your Data',
+                      ),
+                    ),
+                  );
+
+                  users
+                      .add({
+                        'firstname': docFields['firstname'].toString(),
+                        'middlename': docFields['middlename'].toString(),
+                        'lastname': docFields['lastname'].toString(),
+                        'suffix': docFields['suffix'].toString(),
+                        'gender': docFields['gender'].toString(),
+                        'age': docFields['age'].toString(),
+                        'civstatus': docFields['civstatus'].toString(),
+                        'philhealth': docFields['philhealth'].toString(),
+                        'bday': docFields['bday'].toString(),
+                      })
+                      .then((value) => print('Add User'))
+                      .catchError(
+                          (error) => print('Failed to add user: $error'));
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => DisplayData(users: user)),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Color(0xFF26A98A),
+              fixedSize: Size(
+                widget.width * 0.45,
+                widget.height * 0.060,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0),
               ),
             ),
-          ],
-        ),
+            child: Text(
+              'Display Data',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Mulish',
+                fontWeight: FontWeight.w600,
+                fontSize: widget.height * 0.018,
+                decoration: TextDecoration.none,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
-}
 
-//TextFormField
-@immutable
-class PatientFormField extends StatelessWidget {
-  PatientFormField({
-    Key? key,
-    @required this.label,
-  }) : super(key: key);
-
-  final label;
-  late final docField;
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _patientFormField(String label) {
     return TextFormField(
       decoration: InputDecoration(
         labelText: '$label',
       ),
       onChanged: (value) {
-        // log('docField: $docField');
-        // docField = value;
+        switch (label) {
+          case 'First Name':
+            docFields['firstname'] = value;
+            break;
+          case 'Middle Name':
+            docFields['middlename'] = value;
+            break;
+          case 'Last Name':
+            docFields['lastname'] = value;
+            break;
+          case 'Suffix':
+            docFields['suffix'] = value;
+            break;
+          case 'Age':
+            docFields['age'] = value;
+            break;
+        }
       },
       validator: (value) {
         if (value == null || value.isEmpty) {
@@ -279,16 +232,71 @@ class PatientFormField extends StatelessWidget {
       },
     );
   }
+
+  String? getText() {
+    if (date == null) {
+      return 'Birthdate';
+    } else {
+      docFields['bday'] = '${date?.month}/${date?.day}/${date?.year}';
+      return docFields['bday'];
+    }
+  }
+
+  Widget _birthdayPicker() {
+    return InkWell(
+      onTap: () => pickDate(context),
+      child: Container(
+        padding: EdgeInsets.only(bottom: 15, top: 20),
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(width: 1, color: Colors.grey),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              getText().toString(),
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey.shade600,
+              ),
+            ),
+            Icon(
+              Icons.keyboard_arrow_down,
+              color: Colors.grey.shade600,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future pickDate(BuildContext context) async {
+    final initialDate = DateTime.now();
+    final newDate = await showDatePicker(
+      context: context,
+      initialDate: initialDate,
+      firstDate: DateTime(DateTime.now().year - 100),
+      lastDate: DateTime(DateTime.now().year + 1),
+    );
+    if (newDate == null) return;
+
+    setState(() {
+      date = newDate;
+    });
+  }
 }
 
 class PatientDropdown extends StatefulWidget {
   PatientDropdown({
     Key? key,
     @required this.dropList,
+    @required this.label,
   }) : super(key: key);
 
   final dropList;
-  late final docField;
+  final label;
   @override
   _PatientDropdownState createState() => _PatientDropdownState();
 }
@@ -321,7 +329,14 @@ class _PatientDropdownState extends State<PatientDropdown> {
           ),
           onChanged: (value) {
             setState(() => this.dropdownValue = value);
-            // widget.docField = value;
+            switch (widget.label) {
+              case 'Gender':
+                docFields['gender'] = value.toString();
+                break;
+              case 'Civil Status':
+                docFields['civstatus'] = value.toString();
+                break;
+            }
           },
           items: widget.dropList.map<DropdownMenuItem<String>>((String value) {
             return DropdownMenuItem<String>(
@@ -332,70 +347,5 @@ class _PatientDropdownState extends State<PatientDropdown> {
         ),
       ),
     );
-  }
-}
-
-class BirthdayPicker extends StatefulWidget {
-  const BirthdayPicker({Key? key}) : super(key: key);
-
-  @override
-  _BirthdayPickerState createState() => _BirthdayPickerState();
-}
-
-class _BirthdayPickerState extends State<BirthdayPicker> {
-  DateTime? date;
-
-  String getText() {
-    if (date == null) {
-      return 'Birthdate';
-    } else {
-      return '${date?.month}/${date?.day}/${date?.year}';
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => pickDate(context),
-      child: Container(
-        padding: EdgeInsets.only(bottom: 15, top: 20),
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(width: 1, color: Colors.grey),
-          ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              getText(),
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey.shade600,
-              ),
-            ),
-            Icon(
-              Icons.keyboard_arrow_down,
-              color: Colors.grey.shade600,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Future pickDate(BuildContext context) async {
-    final initialDate = DateTime.now();
-    final newDate = await showDatePicker(
-      context: context,
-      initialDate: initialDate,
-      firstDate: DateTime(DateTime.now().year - 100),
-      lastDate: DateTime(DateTime.now().year + 1),
-    );
-    if (newDate == null) return;
-
-    setState(() {
-      date = newDate;
-    });
   }
 }
