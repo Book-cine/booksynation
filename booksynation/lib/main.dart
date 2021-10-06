bb08f20d5da3feb02c3b1efd972814fd0000fa01
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2021-10-07 01:04:29 +0800

    v9.2 Major Bug Fix

--- booksynation/lib/main.dart
+++ booksynation/lib/main.dart
@@ -1,12 +1,58 @@
+import 'package:booksynation/loading.dart';
 import 'package:booksynation/splash.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-Future<void> main() async {
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: true,
-    home: SplashScreen(),
-  ));
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Show error message if initialization failed
+    if (_error) {
+      return AlertDialog(
+        title: const Text('Encountered Error!'),
+      );
+    }
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return MaterialApp(home: Loading());
+    }
+    return MaterialApp(home: SplashScreen());
+  }
 }
