9db4f315bf5d8aed0a11a904337ecc78498291e7
Mervin John C. Tampus <79685609+jdesolate@users.noreply.github.com>
2021-10-12 00:59:27 +0800

    v14.2 CRUD Vaccine Data 10/12/21

--- booksynation/lib/web_pages/web_data/web_vaccines_data.dart
+++ booksynation/lib/web_pages/web_data/web_vaccines_data.dart
@@ -1,15 +1,79 @@
-class VaccineData {
-  String vaccine = '';
-  String dateStart = '';
-  String dateEnd = '';
-  int currentStock = 0;
-  int maxStock = 0;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-  VaccineData(vaccine, dateStart, dateEnd, currentStock, maxStock) {
-    this.vaccine = vaccine;
-    this.dateStart = dateStart;
-    this.dateEnd = dateEnd;
-    this.currentStock = currentStock;
-    this.maxStock = maxStock;
-  }
+class VaccineData {
+  late String uniqueId;
+  late String vaccine;
+  late String dateStart;
+  late String dateEnd;
+  late int currentStock;
+  late int maxStock;
+
+  VaccineData({
+    required this.uniqueId,
+    required this.vaccine,
+    required this.dateStart,
+    required this.dateEnd,
+    required this.currentStock,
+    required this.maxStock,
+  });
+}
+
+var vaccineCollection = FirebaseFirestore.instance.collection('vaccine');
+
+VaccineData vaccineData = VaccineData(
+  uniqueId: '',
+  vaccine: '',
+  dateStart: '',
+  dateEnd: '',
+  currentStock: 0,
+  maxStock: 0,
+);
+
+createVaccineData() async {
+  await FirebaseFirestore.instance.collection('vaccine').add({}).then((value) {
+    print(value.id);
+    vaccineData.uniqueId = value.id;
+    value.set({
+      'UID': vaccineData.uniqueId,
+      'Vaccine': vaccineData.vaccine,
+      'DateStart': vaccineData.dateStart,
+      'DateEnd': vaccineData.dateEnd,
+      'CurrentStock': vaccineData.currentStock,
+      'MaxStock': vaccineData.maxStock,
+    });
+  });
+}
+
+Future readVaccineData() async {
+  await vaccineCollection.doc().get().then((result) {
+    Map<String, dynamic>? value = result.data();
+    vaccineData.vaccine = value?['Vaccine'];
+    vaccineData.dateStart = value?['DateStart'];
+    vaccineData.dateEnd = value?['DateEnd'];
+    vaccineData.currentStock = value?['CurrentStock'];
+    vaccineData.maxStock = value?['MaxStock'];
+  });
+}
+
+updateVaccineData() async {
+  vaccineCollection
+      .doc()
+      .update({
+        'UID': vaccineData.uniqueId,
+        'Vaccine': vaccineData.vaccine,
+        'DateStart': vaccineData.dateStart,
+        'DateEnd': vaccineData.dateEnd,
+        'CurrentStock': vaccineData.currentStock,
+        'MaxStock': vaccineData.maxStock,
+      })
+      .then((value) => print('Vaccine Updated'))
+      .catchError((error) => print('Failed to update vaccine: $error'));
+}
+
+deleteVaccineData() async {
+  vaccineCollection
+      .doc()
+      .delete()
+      .then((value) => print('Vaccine Deleted'))
+      .catchError((error) => print('Failed to delete vaccine: $error'));
 }
