97730cdcf3d63ed31cc4874c3e97de081cbee803
CyberBlaze007 <cyberblazeflo7@gmail.com>
2021-10-22 16:58:51 +0800

    v1.2 Populated the EmailJS IDs

--- booksynation/lib/email_notifs.dart
+++ booksynation/lib/email_notifs.dart
@@ -4,14 +4,16 @@ import 'package:http/http.dart' as http;
 Future sendEmailNotifs({
   required String name,
   required String email,
-  required String subject,
-  required String message,
+  required String vaccine,
+  required String dosage,
+  required String schedule,
 }) async {
-  final serviceId = '';
-  final templateId = '';
-  final userId = '';
+  final serviceId = 'service_uudw07o';
+  final templateId = 'template_zryeo5q';
+  final userId = 'user_tTVTjivQHURFBCECQLga4';
 
-  final url = Uri.parse(); //insert resource url from emailjs insidoe here
+  final url = Uri.parse(
+      'https://api.emailjs.com/api/v1.0/email/send'); //insert resource url from emailjs insidoe here
   final response = await http.post(
     url,
     headers: {
@@ -24,8 +26,9 @@ Future sendEmailNotifs({
       'template_params': {
         'user_name': name,
         'user_email': email,
-        'user_subject': subject,
-        'user_message': message,
+        'user_vaccine': vaccine,
+        'user_dosage': dosage,
+        'user_schedule': schedule,
       }
     }),
   );
